7acfd08b75638d8d1fde079760e9a454ce01a6d8
kashan-ahmad <git@oikashan.com>
2023-11-22 16:40:50 +0500

    feat: added font

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from "./App";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource-variable/bricolage-grotesque";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
